f596caba11bd10f728711b072761ccedc53126de
Hinal Jogani <hinaljogani02@gmail.com>
2024-10-05 11:52:55 +0200

    Testing deploy

--- solarfarmfinder.frontend/src/app/page.js
+++ solarfarmfinder.frontend/src/app/page.js
@@ -73,7 +73,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          Test
         </a>
         <a
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
