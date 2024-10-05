242956f845460b9c07b18562a63e8370d4733f30
bekor <bekor88@gmail.com>
2024-10-05 19:36:40 +0200

    Loading google map

--- solarfarmfinder.frontend/src/app/page.js
+++ solarfarmfinder.frontend/src/app/page.js
@@ -1,23 +1,37 @@
 
 import styles from "./page.module.css";
+// import styles from './GoogleMapLoader.module.css'
 import GoogleMapsLoader from "@/app/GoogleMapLoader";
 import DataView from "@/app/DataView";
 import NavBar from "@/app/NavBar";
 import GISLayer from "@/app/GISLayer";
 
-
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <NavBar/>
-        <GoogleMapsLoader/>
-        <GISLayer/>
-        <DataView/>
-      </main>
-        <footer className={styles.footer}>
+      <div className={styles.page}>
+        <main className={styles.main}>
+            <NavBar/>
+            <div >
+                <GoogleMapsLoader/>
+            </div>
+            <GISLayer/>
+            <DataView/>
+        </main>
+          <footer className={styles.footer}>
 
-        </footer>
-    </div>
+          </footer>
+      </div>
+
+    // <div className={styles.page}>
+    //   <main className={styles.main}>
+    //     {/*<NavBar/>*/}
+    //
+    //     {/*<GISLayer/>*/}
+    //     {/*<DataView/>*/}
+    //   </main>
+    //     <footer className={styles.footer}>
+    //
+    //     </footer>
+    // </div>
   );
 }
