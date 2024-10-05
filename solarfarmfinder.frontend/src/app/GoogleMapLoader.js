d5d1519af530e8db4de653ca6e844a4aa25e0479
bekor <bekor88@gmail.com>
2024-10-05 16:52:04 +0200

    Skeleton for base page and importing google api

---
+++ solarfarmfinder.frontend/src/app/GoogleMapLoader.js
@@ -0,0 +1,11 @@
+'use client'
+
+import { ReactNode } from "react";
+import { Libraries, useJsApiLoader } from "@react-google-maps/api";
+
+const libraries = ['places', 'drawing', 'geometry'];
+export default function GoogleMapLoader() {
+    return(
+        <div></div>
+    );
+}
